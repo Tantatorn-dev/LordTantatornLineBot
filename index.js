548835f9459d6a8928df825f74224facf8dedab3
Tantatorn Suksangwarn <timefear007@gmail.com>
2019-03-11 01:46:31 +0700

    change access token

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 var port = process.env.PORT || 8080;
 
 const config = {
-    channelAccessToken: 'NHqpBWUS4cbo4soab0QkZ7h3PkMDAflgbvqQJ5Z0e38E6b3ceN2HfkmcgUWA19NdqhMrLBCJQ1jF/gu5Gzl35QLzt/fw8ynKLPnjUA0tVfLu1V8G3xgT+qkEJpemZZXqIuC0Jc/V//gF6BZ0JDcCLAdB04t89/1O/w1cDnyilFU=',
+    channelAccessToken: 'Rv1c5HnmDyOXCwZvxAI6G3EPr9uLehyDcExv6MF6o6mAAjkgbVimcZmQ0f7lQD1yqhMrLBCJQ1jF/gu5Gzl35QLzt/fw8ynKLPnjUA0tVfIBSXMYgul9BhD0EIRc3mmPmTWN1zvlcqgN7U9qL5Wl/wdB04t89/1O/w1cDnyilFU=',
     channelSecret: '944bc05f706db53ac55535b1e7dffb9f'
 };
 

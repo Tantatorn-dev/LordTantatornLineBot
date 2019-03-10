9041776e7800077b78e0fa6158cb5cb1580a88ef
Tantatorn Suksangwarn <timefear007@gmail.com>
2019-03-11 01:21:31 +0700

    add new port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const line = require('@line/bot-sdk');
 const express = require('express');
 
+var port = process.env.PORT || 8080;
+
 const config = {
     channelAccessToken: 'NHqpBWUS4cbo4soab0QkZ7h3PkMDAflgbvqQJ5Z0e38E6b3ceN2HfkmcgUWA19NdqhMrLBCJQ1jF/gu5Gzl35QLzt/fw8ynKLPnjUA0tVfLu1V8G3xgT+qkEJpemZZXqIuC0Jc/V//gF6BZ0JDcCLAdB04t89/1O/w1cDnyilFU=',
     channelSecret: '944bc05f706db53ac55535b1e7dffb9f'
@@ -28,4 +30,6 @@ function handleEvent(event) {
     });
 }
   
-app.listen(3000);
+app.listen(port,()=>{
+    console.log("listening");
+});
